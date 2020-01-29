9f1dbe64d3be77152b694a8289bc109453b13705
Erlikhagan <auckun24@gmail.com>
2020-01-29 11:28:16 -0500

    commit whole project

---
+++ BigScouting2020/app/src/main/java/com/example/bigscouting2020/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.example.bigscouting2020.ui.main;
+
+import android.content.Context;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import com.example.bigscouting2020.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    int mNumOfTabs;
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[] {R.string.tab_text_1, R.string.tab_text_2};
+
+
+    public SectionsPagerAdapter(FragmentManager fm, int numOfTabs) {
+        super(fm);
+        this.mNumOfTabs = numOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                Tab1 tab1 = new Tab1();
+                return tab1;
+            case 1:
+                Tab2 tab2 = new Tab2();
+                return tab2;
+            case 2:
+                Tab3 tab3 = new Tab3();
+                return tab3;
+            default:
+                return null;
+        }
+    }
+
+
+
+    @Override
+    public int getCount() {
+       return mNumOfTabs;
+    }
+}
