e5bc97fc9b6ee0c67a9a3a5913e801a3eeda70c6
Erlikhagan <auckun24@gmail.com>
2020-02-11 21:59:19 -0500

    started match scounting auto tab

--- BigScouting2020/app/src/main/java/com/example/bigscouting2020/MainActivity.java
+++ BigScouting2020/app/src/main/java/com/example/bigscouting2020/MainActivity.java
@@ -28,9 +28,9 @@ public class MainActivity extends AppCompatActivity implements Tab1.OnFragmentIn
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.addTab(tabLayout.newTab().setText("Autonomous"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tele-Op"));
+        tabLayout.addTab(tabLayout.newTab().setText("Endgame"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         final SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
