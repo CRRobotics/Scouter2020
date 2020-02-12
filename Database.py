0b151db9e6de0e2f9fcfc6fd48ec7f7bc87172a1
lilrichie <59586372+lilrichie@users.noreply.github.com>
2020-02-11 20:30:55 -0500

    Add files via upload

---
+++ Database.py
@@ -0,0 +1,11 @@
+def localdata(column x, column y):
+
+    import sqlite3
+    conn = conn.cursor(local.sqlite)
+    cur = conn.cursor()
+
+    cur.execute("DROP TABLE IF EXISTS Local)
+    cur.execute("CREATE TABLE Local (x TEXT, y INTEGER)")
+    
+
+    
