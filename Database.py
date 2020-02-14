a6c813a258bebd7655d588f4c6061fd30a574409
lilrichie <59586372+lilrichie@users.noreply.github.com>
2020-02-13 19:48:55 -0500

    Update Database.py

--- Database.py
+++ Database.py
@@ -1,11 +1,19 @@
 def commit_data(cursor, data):
 
     import sqlite3
-    conn = conn.cursor(local.db)
-    cur = conn.cursor()
-
-    cur.execute("DROP TABLE IF EXISTS Local)
-    cur.execute("CREATE TABLE Local (x TEXT, y INTEGER)")
     
-
+    cursor.execute("CREATE TABLE IF NOT EXISTS data_table (crossing BOOLEAN, lowhigh_auto INTEGER, \
+    reach_capacity BOOLEAN, lowhigh INTEGER, spin BOOLEAN, climb BOOLEAN, capacity BOOLEAN)")
+    cursor.execute("INSERT INTO data_table
     
+    local_data = "INSERT INTO data_table (crossing, lowhigh_auto, reach_capacity, lowhigh, spin, climb, capacity) \
+                  VALUES (?, ?, ?, ?, ?, ?, ?)"
+    local_backup = "INSERT INTO data_table (crossing, lowhigh_auto, reach_capacity, lowhigh, spin, climb, capacity) \
+                  VALUES (?, ?, ?, ?, ?, ?, ?)",
+                   
+    conn = sqlite3.connect(database.db)
+    cursor.execute(local_data, data)
+    conn.commit()
+    for row in cursor:
+        print(row)
+    cursor.close()
