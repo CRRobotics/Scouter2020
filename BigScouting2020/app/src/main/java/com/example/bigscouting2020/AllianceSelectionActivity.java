e13a00a0bfaf354d85dafc8e6b791913014fe95b
IHSSAT <patrick.yuan@icsd.k12.ny.us>
2020-03-10 23:24:35 -0400

    delete bullshit

--- BigScouting2020/app/src/main/java/com/example/bigscouting2020/AllianceSelectionActivity.java
+++ BigScouting2020/app/src/main/java/com/example/bigscouting2020/AllianceSelectionActivity.java
@@ -2,13 +2,51 @@ package com.example.bigscouting2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class AllianceSelectionActivity extends AppCompatActivity {
+
+    public final String Level = "LevelConstant";
+    public final String Alliance = "AllianceConstant";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alliance_selection);
+
+        ArrayList<Button> buttons= new ArrayList<>();
+        buttons.add((Button) findViewById(R.id.button2));
+        buttons.add((Button) findViewById(R.id.button3));
+        buttons.add((Button) findViewById(R.id.button4));
+        buttons.add((Button) findViewById(R.id.button5));
+        buttons.add((Button) findViewById(R.id.button6));
+        buttons.add((Button) findViewById(R.id.button7));
+
+        for (View b : buttons)
+        {
+            b.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    startScouter(v);
+                }
+            });
+        }
+    }
+
+    public void startScouter(View v)
+    {
+        Intent intent = new Intent(this, NextScreenActivity.class);
+        String[] type = ((Button) v).getText().toString().split(" ");
+        String level = type[0];
+        String alliance = type[1];
+        System.out.println(level + ":::" + alliance);
+        intent.putExtra(Level, level);
+        intent.putExtra(Alliance, alliance);
+        startActivity(intent);
     }
 }
